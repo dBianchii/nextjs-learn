9d4e1cef651d2fa5bd2a1877851326b2711b948d
Gabriel Bianchi <gdbianchii@gmail.com>
2023-12-13 23:11:35 -0300

    Refactor client component and add dynamic time fetching

--- src/app/static/_components/client.tsx
+++ src/app/static/_components/client.tsx
@@ -1,9 +1,23 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 
 export default function Client() {
-	return (
-		<div className='bg-red-500'>Client component</div>
-	)
+  //Get the lastest pokemon from a server
+  const [time, setTime] = React.useState("");
+
+  React.useEffect(() => {
+    fetch("http://worldtimeapi.org/api/timezone/Africa/Cairo", {
+      cache: "no-store",
+    }) //? The route is static, but fetch is not
+      .then((res) => res.json())
+      .then((data) => setTime(data.datetime));
+  }, []);
+
+  return (
+    <div className="bg-red-500 flex flex-col justify-center items-center">
+      <p>I am a client component, and I am still static!</p>
+      <p className="text-red-200">{time}</p>
+    </div>
+  );
 }
--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import Client from './_components/client'
+import React from "react";
+import Client from "./_components/client";
+import { unstable_noStore } from "next/cache";
 
-export default function Static() {
-	return (
-		<>
-			<div className='flex flex-row justify-center items-center h-full'>This page is a static page</div>
-			<Client/>
-		</>
-	)
+export default async function Static() {
+  //? { cache: "no-store" } here makes this route dynamic!
+  const result = await fetch(
+    "http://worldtimeapi.org/api/timezone/Africa/Cairo"
+  );
+  const awaited = await result.json();
+  const africaCairo = awaited.datetime;
+
+  const result2 = await fetch(
+    "http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires"
+  );
+  const awaited2 = await result2.json();
+  const buenosAires = awaited2.datetime;
+
+  return (
+    <div className="flex flex-col justify-center items-center h-full space-y-4">
+      <h1>This is a static page</h1>
+      <p>The below times are</p>
+      <p className="font-bold">BuenosAires</p> {buenosAires}
+      <p className="font-bold">AfricaCairo</p> {africaCairo}
+      <Client />
+    </div>
+  );
 }
